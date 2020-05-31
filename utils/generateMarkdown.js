60abd4236f41e80edf31dcbfa872e73dfe839313
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-05-31 15:37:08 +0100

    Refactored code into more functial and easily readable format

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,28 +1,89 @@
-function generateMarkdown(data) {
-  // Returns answers from user in README format
+const axios = require("axios");
+
+const generateTile = (title) => `# ${title}`;
+const generateDescription = (description) => {
   return `
+  ## Description
+  
+  ${description}
+  `;
+};
+const generateInstallation = (installation) => {
+  return ` 
+  ## Installation
+  
+  To install all relevant dependencies for this project, run
 
-# ${data.title}
+  ${installation}
+  `;
+};
+const generateUsage = (usage) => {
+  return `
+  ## Usage
 
-### ${data.description}
+  To use this project, 
+  
+  ${usage}`;
+};
+const generateLicence = (license) => {
+  return `
+  ## License
 
-# Installation 
+  ${license}`;
+};
+const generateContributing = (contributing) => {
+  return `
+  ## Contributing 
+  
+  ${contributing}`;
+};
+const generateTests = (tests) => {
+  return `
+  ## Tests
+  
+  ${tests}`;
+};
+const generateCredits = (credits) => {
+  return `
+  ## Credits
+  
+  ${credits}`;
+};
 
-${data.installation}
+//const generateQuestions = (githubProfileUrl, githubEmail) => {
+//  return `
+//  ## Questions
+//
+//  ${githubProfileUrl}
+//  - ${githubEmail}`;
+//};
 
-# Usage
+function generateMarkdown(data) {
+  // Declared variables for markdown sections
+  const title = data.title;
+  const description = data.description;
+  const installation = data.installation;
+  const usage = data.usage;
+  const license = data.license;
+  const contributing = data.contributing;
+  const tests = data.tests;
+  const credits = data.credits;
+  //const githubProfileUrl = data.gitHubProfileUrl;
+  //const githubEmail = data.email;
 
-${data.usage}
-
-# Credits 
-
-${data.credits}
-
-# License
-
-${data.license}
-
-`;
+  // Returns answers from user into README format
+  const finalReadme = `
+  ${generateTile(title)}
+  ${generateDescription(description)}
+  ${generateInstallation(installation)}
+  ${generateUsage(usage)}
+  ${generateLicence(license)}
+  ${generateContributing(contributing)}  
+  ${generateTests(tests)}
+  ${generateCredits(credits)}
+  `;
+  console.log(finalReadme);
+  return finalReadme;
 }
 
 // Exports module so it can be used within other files
